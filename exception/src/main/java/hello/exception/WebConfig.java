d58214b51e0f591dbf26d7d9997d58cdb808a489
psora <psora93@gmail.com>
2024-08-05 22:20:42 +0900

    feat:Exception with Api

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -26,13 +26,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new LogInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "*.ico","/error", "/error-page/**");  // 오류 페이지 경로
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(new LogInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/css/**", "*.ico","/error", "/error-page/**");  // 오류 페이지 경로
+//    }
 
     //@Bean
     public FilterRegistrationBean logFilter(){
---
+++ exception/src/main/java/hello/exception/api/ApiExceptionConroller.java
@@ -0,0 +1,41 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : hello.exception.api
+ * fileName       : ApiExceptionConroller
+ * author         : Sora
+ * date           : 2024-08-05
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-05        Sora       최초 생성
+ */
+@Slf4j
+@RestController
+public class ApiExceptionConroller {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMemgers(@PathVariable("id") String id){
+
+        if(id.equals("ex")){
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String memberId;
+        private String name;
+    }
+}
